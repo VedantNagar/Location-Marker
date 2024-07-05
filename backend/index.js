0428c26bfd1f6a8979ff584183aaa73d9f317150
Vedant <vedantnagar21@gmail.com>
2024-07-05 21:38:43 +0530

    chore: Add database connection and JSON body parsing

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./database/connectDB.js";
 const app = express();
-
 dotenv.config();
+
+app.use(express.json());
 
 const port = process.env.PORT || 3000;
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
