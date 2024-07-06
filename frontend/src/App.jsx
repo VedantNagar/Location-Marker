36678a91e095c5a7ed5c9f4a5d669c63d3da6a5b
Vedant <vedantnagar21@gmail.com>
2024-07-06 12:25:36 +0530

    chore: Update frontend dependencies and add Mapbox integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
+import * as React from "react";
+import Map, { Marker } from "react-map-gl";
+
 function App() {
   return (
-    <>
-      <h1>Hello </h1>
-    </>
+    <Map
+      mapboxAccessToken="pk.eyJ1IjoidmVkYW50MjEiLCJhIjoiY2x5OW0wOXZyMHR1dzJ2b2hxZTM4d2g3MSJ9.fK8JHGe7_RNazEam66wTCg"
+      initialViewState={{
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 14,
+      }}
+      style={{ width: "100%", height: "100%" }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    />
   );
 }
-
 export default App;
