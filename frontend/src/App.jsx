ee6890daefaa062a2f2be012116e4606217a1852
Vedant <vedantnagar21@gmail.com>
2024-07-06 16:06:57 +0530

    chore: Add popup for pin information on map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import Map, { Marker } from "react-map-gl";
+import Map, { Marker, Popup } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { FaMapPin } from "react-icons/fa6";
+import { FaStar } from "react-icons/fa";
 
 function App() {
   const [viewState, setViewState] = useState({
@@ -37,6 +38,40 @@ function App() {
             style={{ fontSize: `${viewState.zoom * 5}px` }}
           />
         </Marker>
+        <Popup
+          className="w-96 h-64"
+          latitude={pinPosition.latitude}
+          longitude={pinPosition.longitude}
+          closeButton={true}
+          closeOnClick={false}
+          anchor="left"
+        >
+          <div class="bg-white shadow-md rounded-lg p-1">
+            <label class="text-sm text-red-500 border-b-2 border-red-500">
+              Place
+            </label>
+            <h4 class="place text-xl font-bold">Random Home</h4>
+            <label class="text-sm text-red-500 border-b-2 border-red-500">
+              Review
+            </label>
+            <h4 class="text-base">5/5</h4>
+            <label class="text-sm text-red-500 border-b-2 border-red-500">
+              Rating
+            </label>
+            <div class="flex items-center p-1 pl-0">
+              <FaStar class="text-yellow-500 text-2xl" />
+              <FaStar class="text-yellow-500 text-2xl" />
+              <FaStar class="text-yellow-500 text-2xl" />
+            </div>
+            <label class="text-sm text-red-500 border-b-2 border-red-500">
+              Information
+            </label>
+            <div class="username text-sm">
+              Random Information is being entered here
+            </div>
+            <span class="date text-xs">1 Hour Ago</span>
+          </div>
+        </Popup>
       </Map>
     </div>
   );
