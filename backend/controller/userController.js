14b1ce1c464d70cf7f5b4cb03aca95e6e44df664
Vedant <vedantnagar21@gmail.com>
2024-07-09 20:52:57 +0530

    chore: Add user CRUD routes and controller methods

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -12,6 +12,7 @@ export const register = async (req, res) => {
       password: hashedPassword,
     });
     await newUser.save();
+    res.json(newUser);
     res.status(201).json("User has been registered");
   } catch (error) {
     res.status(500).json(error);
@@ -29,10 +30,44 @@ export const login = async (req, res) => {
     if (!correctPassword) {
       return res.status(401).json("Wrong password");
     }
-    const { password: pass, ...others } = user._doc;
+    const { password: pass, ...userInfo } = user._doc;
     const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY);
     res.cookie("token", token, { httpOnly: true }); //cannot be accessed via JavaScript in the browser
-    res.status(200).json({ others, token });
+    res.status(200).json({ userInfo, token });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+export const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+export const getUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findById(id);
+    if (!user) return res.status(404).json("User not found");
+    const { password, ...userInfo } = user._doc;
+    res.status(200).json(userInfo);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findByIdAndDelete(id);
+    if (!user) {
+      return res.status(404).json("User not found");
+    }
+    res.status(200).json("User has been deleted");
   } catch (error) {
     res.status(500).json(error);
   }
--- backend/router/userRoutes.js
+++ backend/router/userRoutes.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { register, login } from "../controller/userController.js";
+import {
+  register,
+  login,
+  deleteUser,
+  getUser,
+  getUsers,
+} from "../controller/userController.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.delete("/:id", deleteUser);
+router.get("/getUser/:id", getUser);
+router.get("/getUsers", getUsers);
 
 export default router;
