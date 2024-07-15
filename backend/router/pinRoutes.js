fb2dfb1a5b92f31a94bde1112a7d2ab134ec9879
Vedant <vedantnagar21@gmail.com>
2024-07-15 09:37:32 +0530

    refactor: Update getPins route to include pin ID in the URL

--- backend/router/pinRoutes.js
+++ backend/router/pinRoutes.js
@@ -4,7 +4,7 @@ import { createPin, deletePins, getPins } from "../controller/pinController.js";
 const router = express.Router();
 
 router.post("/", createPin);
-router.get("/", getPins);
+router.get("/pins/:id", getPins);
 router.delete("/:id", deletePins);
 
 export default router;
